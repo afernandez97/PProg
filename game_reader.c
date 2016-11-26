47304f0b35addbe6347d86189ca3f7b20efd263e
Adrián Fernández <adrian-fernandez-amador@users.noreply.github.com>
2016-11-26 18:38:52 +0100

    Doxygen

--- game_reader.c
+++ game_reader.c
@@ -1,25 +1,31 @@
-/* =================================================================== 
-   File: game_reader.c
-   Version: 4.1
-   Date: 11-11-2016
-   Author: Guillermo Rodriguez and Alejandro Sanchez
+/**
+@file game_reader.c
+@version 4.0
+@date 11-11-2016
+@authors Guillermo Rodriguez and Alejandro Sanchez
 
-   Description:
-    It implements the game reader.
+@brief
+It implements the game reader.
 
-   Revision history:
-    Sept. 30, 2016  Version 1.0 (initial release)
-    Sept. 30, 2016  Version 2.0 
-      Commented the file.
-		Oct. 27,	2016	Version 3.0
-			Created functions "game_load_objects" and "game_add_object".
-      Modified function "game_load_spaces".
-    Nov. 05, 2016   Version 4.0
-      Modified functions after making "Game" structure private.
-    Nov. 11, 2016   Version 4.1
-      Created functions "game_load_links" and "game_add_link".  
-=================================================================== */
-
+@version
+Sept. 30, 2016  Version 1.0 (initial release)
+@version
+Sept. 30, 2016  Version 2.0 
+  Commented the file.
+@version
+Oct. 27,  2016  Version 3.0
+  Created functions "game_load_objects" and "game_add_object".
+  Modified function "game_load_spaces".
+@version
+Nov. 05, 2016   Version 4.0
+  Modified functions after making "Game" structure private.
+@version
+Nov. 11, 2016   Version 4.1
+  Created functions "game_load_links" and "game_add_link".  
+@version
+Nov. 26, 2016 Version 5.0
+  Updated headers to use Doxygen.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,38 +39,36 @@
 
 
 /*** Public functions definition ***/
-/* ----------------------------------------------------------------------------
-   Function: game_add_space
-   Date: 30-09-2016 
-   Author: Alejandro Sanchez
 
-   Description: 
-    Adds a space to a game.
+/**
+@date 30-09-2016 
+@author Alejandro Sanchez
 
-   Input: 
-    Game *game: the game where you add the space.
-    Space *space: the space you want to add to the game.  
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@brief game_add_space
+Add a space to a game.
+
+@param Game *game: the game where you add the space.
+@param Space *space: the space you want to add to the game.  
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_add_space(Game *game, Space *space){
-  int i = 0;  /* Initialize the counter */
+  int i = 0;  /*!< Initialize the counter */
 
-  if(!game || !space){  /* Check that the inputs are not empty */
+  if(!game || !space){  /*!< Check that the inputs are not empty */
     return ERROR;
   }
 
-  /* Increase the counter until finding an empty space */
+  /*!< Increase the counter until finding an empty space */
   while(i < MAX_SPACES && game_get_space_at_position(game, i) != NULL){
     i++;
   }
 
-  /* Check if every space is not empty */
+  /*!< Check if every space is not empty */
   if(i >= MAX_SPACES){
     return ERROR;
   }
 
-  /* Set the new space */
+  /*!< Set the new space */
   if(game_set_space_at_position(game, space, i) == ERROR){
     return ERROR;
   }
@@ -74,20 +78,17 @@ STATUS game_add_space(Game *game, Space *space){
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_load_spaces
-   Date: 27-10-2016 
-   Author: Alejandro Sanchez
+/**
+@date 27-10-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Loads the spaes from a file.
+@brief game_load_spaces
+Loads a space from a file.
 
-   Input: 
-    Game *game: the game where you want to load the spaces.
-    char *filename: the file that contains the spaces.   
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you want to load the spaces.
+@param char *filename: the file that contains the spaces.   
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_load_spaces(Game *game, char *filename){
   FILE *file = NULL;
   char line[WORD_SIZE] = "";
@@ -99,18 +100,18 @@ STATUS game_load_spaces(Game *game, char *filename){
   Space *space = NULL;
   STATUS status = OK;
   
-  if(!game || !filename){ /* Check that the inputs are not empty */
+  if(!game || !filename){ /*!< Check that the inputs are not empty */
     return ERROR;
   }
   
   strcat(filename,"_spaces.dat");
   
-  file = fopen(filename, "r");   /* Open the file where the spaces are */
+  file = fopen(filename, "r");   /*!< Open the file where the spaces are */
   if(!file){
     return ERROR;
   }
   
-  /* Read each line of the file and get the id and the links of each space */
+  /*!< Read each line of the file and get the id and the links of each space */
   while(fgets(line, WORD_SIZE, file)){
     if(strncmp("#s:", line, 3) == 0){
       toks = strtok(line + 3, "|");
@@ -137,72 +138,69 @@ STATUS game_load_spaces(Game *game, char *filename){
 #ifdef DEBUG 
       printf("Leido: %ld|%s|%ld|%ld|%ld|%ld|%s|%s\n", id, name, north, east, south, west, desc, gdesc);
 #endif
-      space = space_create(id); /* Create the space */
+      space = space_create(id); /*!< Create the space */
       if(space != NULL){
-        /* Set the name to the space */  
+        /*!< Set the name to the space */  
         space_set_name(space, name);
 
-        /* Set the links to the space */  
+        /*!< Set the links to the space */  
         space_set_north(space, north);
         space_set_east(space, east);
         space_set_south(space, south);
         space_set_west(space, west);
 
-        /* Set the graphic description to the space */ 
+        /*!< Set the graphic description to the space */ 
 				space_set_gdesc(space, gdesc);
         
-				/* Set the description to the space */ 
+				/*!< Set the description to the space */ 
 				space_set_desc(space, desc);
         
-        /* Add the space to the game */
+        /*!< Add the space to the game */
         game_add_space(game, space);  
       }
-    } /* if(strncmp("#s:", line, 3) == 0) */
-  } /* while */
+    } /*!< if(strncmp("#s:", line, 3) == 0) */
+  } /*!< while */
   
-  if(ferror(file)){ /* Test the error indicator for the file */ 
+  if(ferror(file)){ /*!< Test the error indicator for the file */ 
     status = ERROR;
   }
   
-  fclose(file); /* Close the file */
+  fclose(file); /*!< Close the file */
   
   return status;
 }
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_add_object
-   Date: 27-10-2016 
-   Author: Ricardo Riol
+/**
+@date 27-10-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Adds an object to a game.
+@brief game_add_object
+Adds an object to a game.
 
-   Input: 
-    Game *game: the game where you add the object.
-    Object *object: the object you want to add to the game.  
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you add the object.
+@param Object *object: the object you want to add to the game.  
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_add_object(Game *game, Object *object){
-  int i = 0;  /* Initialize the counter */
+  int i = 0;  /*!< Initialize the counter */
 
-  if(!game || !object){  /* Check that the inputs are not empty */
+  if(!game || !object){  /*!< Check that the inputs are not empty */
     return ERROR;
   }
 
-  /* Increase the counter until finding an empty object */
+  /*!< Increase the counter until finding an empty object */
   while(i < MAX_OBJECTS && game_get_object_at_position(game, i) != NULL){
     i++;
   }
 
-  /* Check if every object is not empty */
+  /*!< Check if every object is not empty */
   if(i >= MAX_OBJECTS){
     return ERROR;
   }
 
-  /* Set the new object */
+  /*!< Set the new object */
   if(game_set_object_at_position(game, object, i) == ERROR){
     return ERROR;
 	}
@@ -212,20 +210,17 @@ STATUS game_add_object(Game *game, Object *object){
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_load_objects
-   Date: 27-10-2016 
-   Author: Alejandro Sanchez
+/**
+@date 27-10-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Loads the objects from a file.
+@brief game_load_objects
+Loads the objects from a file.
 
-   Input: 
-    Game *game: the game where you want to load the objects.
-    char *filename: the file that contains the objects.   
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you want to load the objects.
+@param char *filename: the file that contains the spaces.   
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_load_objects(Game *game, char *filename){
   FILE *file = NULL;
   char line[WORD_SIZE] = "";
@@ -237,18 +232,18 @@ STATUS game_load_objects(Game *game, char *filename){
   Space *space = NULL;
   STATUS status = OK;
   
-  if(!game || !filename){ /* Check that the inputs are not empty */
+  if(!game || !filename){ /*!< Check that the inputs are not empty */
     return ERROR;
   }
   
   strcat(filename,"_objects.dat");
   
-  file = fopen(filename, "r");   /* Open the file where the objects are */
+  file = fopen(filename, "r");   /*!< Open the file where the objects are */
   if(!file){
     return ERROR;
   }
   
-  /* Read each line of the file and get the id, the name and the location of the objects */
+  /*!< Read each line of the file and get the id, the name and the location of the objects */
   while(fgets(line, WORD_SIZE, file)){
     if(strncmp("#o:", line, 3) == 0){
       toks = strtok(line + 3, "|");
@@ -264,23 +259,23 @@ STATUS game_load_objects(Game *game, char *filename){
 #ifdef DEBUG 
       printf("Leido: %ld|%s|%ld|%s\n", id, name, location, desc);
 #endif
-      object = object_create(id); /* Create the object */
+      object = object_create(id); /*!< Create the object */
       if(object != NULL){
-        /* Set the name to the object */  
+        /*!< Set the name to the object */  
         object_set_name(object, name);
 
-        /* Set the location to the object */  
+        /*!< Set the location to the object */  
 				object_set_location(object, location);
 				
-				/* Set the description to the object */ 
+				/*!< Set the description to the object */ 
 				object_set_desc(object, desc);
         		
 
-        /* Add the object to the game */
+        /*!< Add the object to the game */
         game_add_object(game, object);  
 
 
-        /* Add the object to the set of objects of its location */
+        /*!< Add the object to the set of objects of its location */
         space = game_get_space(game, location);
         if(space != NULL){
           space_add_object(space, id);
@@ -288,52 +283,49 @@ STATUS game_load_objects(Game *game, char *filename){
 
         
       }
-    } /* if(strncmp("#o:", line, 3) == 0) */
-  } /* while */
+    } /*!< if(strncmp("#o:", line, 3) == 0) */
+  } /*!< while */
   
-  if(ferror(file)){ /* Test the error indicator for the file */ 
+  if(ferror(file)){ /*!< Test the error indicator for the file */ 
     status = ERROR;
   }
   
-  fclose(file); /* Close the file */
+  fclose(file); /*!< Close the file */
   
   return status;
 }
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_add_link
-   Date: 13-11-2016 
-   Author: Alejandro Sanchez
+/**
+@date 13-11-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Adds an link to a game.
+@brief game_add_link
+Adds an link to a game.
 
-   Input: 
-    Game *game: the game where you add the link.
-    Link *link: the link you want to add to the game.  
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you add the link.
+@param Link *link: the link you want to add to the game.  
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_add_link(Game *game, Link *link){
-  int i = 0;  /* Initialize the counter */
+  int i = 0;  /*!< Initialize the counter */
 
-  if(!game || !link){  /* Check that the inputs are not empty */
+  if(!game || !link){  /*!< Check that the inputs are not empty */
     return ERROR;
   }
 
-  /* Increase the counter until finding an empty link */
+  /*!< Increase the counter until finding an empty link */
   while(i < MAX_LINKS && game_get_link_at_position(game, i) != NULL){
     i++;
   }
 
-  /* Check if every link is not empty */
+  /*!< Check if every link is not empty */
   if(i >= MAX_LINKS){
     return ERROR;
   }
 
-  /* Set the new link */
+  /*!< Set the new link */
   if(game_set_link_at_position(game, link, i) == ERROR){
     return ERROR;
   }
@@ -343,20 +335,17 @@ STATUS game_add_link(Game *game, Link *link){
 
 
 
-/* --------------------------------------------------------------------
-   Function: game_load_links
-   Date: 10-11-2016
-   Author: Ricardo Riol
- 
-   Description:
- 	  Loads the links from a file.
- 	
-   Input:
-    Game *game: the game where you want to load the links.
-    char *filename: the file that contains the links. 
-   Output:
-    STATUS: OK if you do the operation well and ERROR in other cases.
- ------------------------------------------------------------------- */
+/**
+@date 10-11-2016
+@author Ricardo Riol
+
+@brief game_load_links
+Loads the links from a file.
+
+@param Game *game: the game where you want to load the links.
+@param char *filename: the file that contains the links. 
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_load_links(Game *game, char *filename){
 	FILE *file = NULL;
 	char line[WORD_SIZE] = "";
@@ -370,17 +359,17 @@ STATUS game_load_links(Game *game, char *filename){
   STATUS status = OK;
 
 
-	if(!game || !filename){    /* Check that the inputs are not empty */
+	if(!game || !filename){    /*!< Check that the inputs are not empty */
 		return ERROR;
 	}
         strcat(filename,"_links.dat");
 	
-        file = fopen(filename, "r");	/* Open the file where the links are */
+        file = fopen(filename, "r");	/*!< Open the file where the links are */
 	if(!file){				
 		return ERROR;
 	}
 
-  /* Read each line of the file and get the id, the name, the spaces and the state of the links */
+  /*!< Read each line of the file and get the id, the name, the spaces and the state of the links */
 	while(fgets(line, WORD_SIZE, file)){
 		if(strncmp("#l:", line, 3) == 0){
 			toks = strtok(line + 3, "|");
@@ -398,25 +387,25 @@ STATUS game_load_links(Game *game, char *filename){
 	printf("Leido: %ld|%s|%ld|%ld|%d\n", id, name, space_id1, space_id2, link_state);
 #endif
 	
-     	link = link_create(id);	/* Create the link */
+     	link = link_create(id);	/*!< Create the link */
    		if(link != NULL){
-        /* Set the name to the link */			
+        /*!< Set the name to the link */			
   			link_set_name(link, name);
 
-        /* Set the first space to the link */
+        /*!< Set the first space to the link */
         link_set_space1(link, space_id1);
 
-        /* Set the second space to the link */
+        /*!< Set the second space to the link */
         link_set_space2(link, space_id2);
 
-        /* Set the state to the link */
+        /*!< Set the state to the link */
 	   		link_set_state(link, link_state);
 
-        /* Add the link to the game */
+        /*!< Add the link to the game */
         game_add_link(game, link);  
       }
-    } /* if(strncmp("#l:", line, 3) == 0) */
-  } /* while */
+    } /*!< if(strncmp("#l:", line, 3) == 0) */
+  } /*!< while */
   
   if(ferror(file)){
   	status = ERROR;
--- game_reader.h
+++ game_reader.h
@@ -1,127 +1,116 @@
-/* =================================================================== 
-   File: game_reader.h
-   Version: 4.0
-   Date: 11-11-2016
-   Author: Guillermo Rodriguez and Alejandro Sanchez
+/**
+File: game_reader.h
+@version 5.0
+@date 11-11-2016
+@authors Guillermo Rodriguez and Alejandro Sanchez
 
-   Description:
-    It defines the game reader.
+@brief
+It defines the game reader.
 
-   Revision history:
-    Sept. 30, 2016 Version 1.0 (initial release)
-    Sept. 30, 2016  Version 2.0 
-    	Commented the file.
-		Oct.	27,	2016	Version 3.0
-			Created functions "game_load_objects" and "game_add_object".
-    Nov. 11, 2016   Version 4.0
-      Created functions "game_load_links" and "game_add_link".
-=================================================================== */
+@version
+Sept. 30, 2016 Version 1.0 (initial release)
+@version
+Sept. 30, 2016  Version 2.0 
+	Commented the file.
+@version
+Oct.	27,	2016	Version 3.0
+	Created functions "game_load_objects" and "game_add_object".
+@version
+Nov. 11, 2016   Version 4.0
+  Created functions "game_load_links" and "game_add_link".
+@version
+Nov. 26, 2016 Version 5.0
+  Updated headers to use Doxygen.
+*/
 #ifndef GAME_READER_H
 #define GAME_READER_H
 #include "game.h"
 
 
 /*** Public functions description ***/
-/* ----------------------------------------------------------------------------
-   Function: game_add_space
-   Date: 30-09-2016 
-   Author: Alejandro Sanchez
 
-   Description: 
-    Add a space to a game.
+/**
+@date 30-09-2016 
+@author Alejandro Sanchez
 
-   Input: 
-    Game *game: the game where you add the space.
-    Space *space: the space you want to add to the game.  
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@brief game_add_space
+Add a space to a game.
+
+@param Game *game: the game where you add the space.
+@param Space *space: the space you want to add to the game.  
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_add_space(Game *game, Space *space);
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_load_spaces
-   Date: 27-10-2016 
-   Author: Alejandro Sanchez
+/**
+@date 27-10-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Loads a space from a file.
+@brief game_load_spaces
+Loads a space from a file.
 
-   Input: 
-    Game *game: the game where you want to load the spaces.
-    char *filename: the file that contains the spaces.   
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you want to load the spaces.
+@param char *filename: the file that contains the spaces.   
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_load_spaces(Game* game, char* filename); 
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_add_object
-   Date: 27-10-2016 
-   Author: Alejandro Sanchez
+/**
+@date 27-10-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Adds an object to a game.
+@brief game_add_object
+Adds an object to a game.
 
-   Input: 
-    Game *game: the game where you add the object.
-    Object *object: the object you want to add to the game.  
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you add the object.
+@param Object *object: the object you want to add to the game.  
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_add_object(Game *game, Object *object);
 
 
 
-/* ----------------------------------------------------------------------------
-   Function: game_load_objects
-   Date: 27-10-2016 
-   Author: Alejandro Sanchez
+/**
+@date 27-10-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Loads the objects from a file.
+@brief game_load_objects
+Loads the objects from a file.
 
-   Input: 
-    Game *game: the game where you want to load the objects.
-    char *filename: the file that contains the spaces.   
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you want to load the objects.
+@param char *filename: the file that contains the spaces.   
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_load_objects(Game *game, char *filename);
 
-/* ----------------------------------------------------------------------------
-   Function: game_add_link
-   Date: 13-11-2016 
-   Author: Alejandro Sanchez
+/**
+@date 13-11-2016 
+@author Alejandro Sanchez
 
-   Description: 
-    Adds an link to a game.
+@brief game_add_link
+Adds an link to a game.
 
-   Input: 
-    Game *game: the game where you add the link.
-    Link *link: the link you want to add to the game.  
-   Output: 
-    STATUS: OK if you do the operation well and ERROR in other cases.
-   --------------------------------------------------------------------------- */
+@param Game *game: the game where you add the link.
+@param Link *link: the link you want to add to the game.  
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_add_link(Game *game, Link *link);
 
-/* --------------------------------------------------------------------
-   Function: game_load_links
-   Date: 10-11-2016
-   Author: Ricardo Riol
- 
-   Description:
-    Loads the links from a file.
-  
-   Input:
-    Game *game: the game where you want to load the links.
-    char *filename: the file that contains the links. 
-   Output:
-    STATUS: OK if you do the operation well and ERROR in other cases.
- ------------------------------------------------------------------- */
+/**
+@date 10-11-2016
+@author Ricardo Riol
+
+@brief game_load_links
+Loads the links from a file.
+
+@param Game *game: the game where you want to load the links.
+@param char *filename: the file that contains the links. 
+@return STATUS: OK if you do the operation well and ERROR in other cases.
+*/
 STATUS game_load_links(Game *game, char *filename);
 
 #endif
