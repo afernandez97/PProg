8cfd9428948c2f111b45daedc9be9234b927476d
Jandrov <jandrov.97@gmail.com>
2016-12-02 02:21:50 +0100

    Update space_test.c

--- space_test.c
+++ space_test.c
@@ -1,7 +1,7 @@
 /**
 @file space_test.c
-@version 1.0
-@date 23-11-2016
+@version 2.0
+@date 01-12-2016
 @author Adrián Fernández
 
 @brief
@@ -10,6 +10,9 @@ functions implemented in space.c.
 
 @version
 	Nov. 23, 2016  Version 1.0 (initial release)
+@version
+	Dec. 01, 2016  Version 2.0
+		Added tests for new fields up, down and illuminated.
 */
 
 #include <stdio.h>
@@ -60,31 +63,44 @@ int main(int argc, char **argv){
 	if (all || test == 25) test2_space_set_west();
 	if (all || test == 26) test1_space_get_west();
 	if (all || test == 27) test2_space_get_west();
-	if (all || test == 28) test1_space_add_object();
-	if (all || test == 29) test2_space_add_object();
-	if (all || test == 30) test3_space_add_object();
-	if (all || test == 31) test1_space_del_object();
-	if (all || test == 32) test2_space_del_object();
-	if (all || test == 33) test3_space_del_object();
-	if (all || test == 34) test1_space_get_object();
-	if (all || test == 35) test2_space_get_object();
-	if (all || test == 36) test1_space_is_object();
-	if (all || test == 37) test2_space_is_object();
-	if (all || test == 38) test3_space_is_object();
-	if (all || test == 39) test1_space_set_desc();
-	if (all || test == 40) test2_space_set_desc();
-	if (all || test == 41) test3_space_set_desc();
-	if (all || test == 42) test1_space_get_desc();
-	if (all || test == 43) test2_space_get_desc();
-	if (all || test == 44) test1_space_set_gdesc();
-	if (all || test == 45) test2_space_set_gdesc();
-	if (all || test == 46) test3_space_set_gdesc();
-	if (all || test == 47) test1_space_get_gdesc();
-	if (all || test == 48) test2_space_get_gdesc();
-	if (all || test == 39) test1_space_print();
-	if (all || test == 50) test2_space_print();
-	if (all || test == 51) test1_space_print_gdesc();
-	if (all || test == 52) test2_space_print_gdesc();
+	if (all || test == 28) test1_space_set_up();
+	if (all || test == 29) test2_space_set_up();
+	if (all || test == 30) test1_space_get_up();
+	if (all || test == 31) test2_space_get_up();
+	if (all || test == 32) test1_space_set_down();
+	if (all || test == 33) test2_space_set_down();
+	if (all || test == 34) test1_space_get_down();
+	if (all || test == 35) test2_space_get_down();
+	if (all || test == 36) test1_space_add_object();
+	if (all || test == 37) test2_space_add_object();
+	if (all || test == 38) test3_space_add_object();
+	if (all || test == 39) test1_space_del_object();
+	if (all || test == 40) test2_space_del_object();
+	if (all || test == 41) test3_space_del_object();
+	if (all || test == 42) test1_space_get_object();
+	if (all || test == 43) test2_space_get_object();
+	if (all || test == 44) test1_space_is_object();
+	if (all || test == 45) test2_space_is_object();
+	if (all || test == 46) test3_space_is_object();
+	if (all || test == 47) test1_space_set_desc();
+	if (all || test == 48) test2_space_set_desc();
+	if (all || test == 49) test3_space_set_desc();
+	if (all || test == 50) test1_space_get_desc();
+	if (all || test == 51) test2_space_get_desc();
+	if (all || test == 52) test1_space_set_gdesc();
+	if (all || test == 53) test2_space_set_gdesc();
+	if (all || test == 54) test3_space_set_gdesc();
+	if (all || test == 55) test1_space_get_gdesc();
+	if (all || test == 56) test2_space_get_gdesc();
+	if (all || test == 57) test1_space_print_gdesc();
+	if (all || test == 58) test2_space_print_gdesc();
+	if (all || test == 59) test1_space_set_illumination();
+	if (all || test == 60) test2_space_set_illumination();
+	if (all || test == 61) test1_space_is_illuminated();
+	if (all || test == 62) test2_space_is_illuminated();
+	if (all || test == 63) test1_space_print();
+	if (all || test == 64) test2_space_print();
+
 
 	return 0;
 }
@@ -624,6 +640,165 @@ void test2_space_get_west(){
 	return;
 }
 
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test1_space_set_up
+Tests if you can successfully set the up of a space.
+
+@param
+@return
+*/
+void test1_space_set_up(){
+	Space *space = NULL;
+	Id id = 1;
+
+	space = space_create(id);
+	FUNCTION_IS_CORRECT(space_set_up(space, id) == OK);
+	space_destroy(space);
+	return;
+}
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test2_space_set_up
+Tests if you can set the up of an uninitialised space.
+
+@param
+@return
+*/
+void test2_space_set_up(){
+	Space *space = NULL;
+	Id id = 1;
+
+	FUNCTION_IS_CORRECT(space_set_up(space, id) == ERROR);
+	return;
+}
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test1_space_get_up
+Tests if you can successfully get the up of a space.
+
+@param
+@return
+*/
+void test1_space_get_up(){
+	Space *space = NULL;
+	Id id = 1;
+
+	space = space_create(id);
+	space_set_up(space, id);
+	FUNCTION_IS_CORRECT(space_get_up(space) != NO_ID);
+	space_destroy(space);
+	return;
+}
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test2_space_get_up
+Tests if you can get the up field of an uninitialised space.
+
+@param
+@return
+*/
+void test2_space_get_up(){
+	Space *space = NULL;
+	Id id = 1;
+
+	space_set_up(space, id);
+	FUNCTION_IS_CORRECT(space_get_up(space) == NO_ID);
+	return;
+}
+
+
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test1_space_set_down
+Tests if you can successfully set the down of a space.
+
+@param
+@return
+*/
+void test1_space_set_down(){
+	Space *space = NULL;
+	Id id = 1;
+
+	space = space_create(id);
+	FUNCTION_IS_CORRECT(space_set_down(space, id) == OK);
+	space_destroy(space);
+	return;
+}
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test2_space_set_down
+Tests if you can set the down of an uninitialised space.
+
+@param
+@return
+*/
+void test2_space_set_down(){
+	Space *space = NULL;
+	Id id = 1;
+
+	FUNCTION_IS_CORRECT(space_set_down(space, id) == ERROR);
+	return;
+}
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test1_space_get_down
+Tests if you can successfully get the down of a space.
+
+@param
+@return
+*/
+void test1_space_get_down(){
+	Space *space = NULL;
+	Id id = 1;
+
+	space = space_create(id);
+	space_set_down(space, id);
+	FUNCTION_IS_CORRECT(space_get_down(space) != NO_ID);
+	space_destroy(space);
+	return;
+}
+
+/**
+@author Alejandro Sanchez
+@date Dec. 01, 2016
+
+@brief test2_space_get_down
+Tests if you can get the down field of an uninitialised space.
+
+@param
+@return
+*/
+void test2_space_get_down(){
+	Space *space = NULL;
+	Id id = 1;
+
+	space_set_down(space, id);
+	FUNCTION_IS_CORRECT(space_get_down(space) == NO_ID);
+	return;
+}
+
 /**
 @author Adrián Fernández
 @date Nov. 23, 2016
@@ -1096,6 +1271,78 @@ void test2_space_print_gdesc(){
 }
 
 /**
+@author Guillermo Rodriguez
+@date Dec. 01, 2016
+
+@brief test1_space_set_light
+Tests if you set an illumination for an unitialized space
+
+@param
+@return
+*/
+void test1_space_set_illumination(){
+	Space *space = NULL;
+
+	FUNCTION_IS_CORRECT(space_set_illumination(space,TRUE) == ERROR);
+	return;
+}
+/**
+
+@author Guillermo Rodriguez
+@date Dec. 01, 2016
+
+@brief test2_space_set_illumination
+Tests if you set an illumination correctly
+
+@param
+@return
+*/
+void test2_space_set_illumination(){
+	Space *space = NULL;
+  Id id = 1;
+  space = space_create(id);
+  FUNCTION_IS_CORRECT(space_set_illumination(space,TRUE) == OK);
+	return;
+}
+
+/**
+@author Guillermo Rodriguez
+@date Dec. 01, 2016
+
+@brief test1_space_is_illuminated
+Tests if can check if an unitialized space is illuminated or not
+
+@param
+@return
+*/
+void test1_space_is_illuminated(){
+	Space *space = NULL;
+
+	FUNCTION_IS_CORRECT(space_is_illuminated(space) == FALSE);
+	return;
+}
+
+/**
+@author Guillermo Rodriguez
+@date Dec. 01, 2016
+
+@brief test2_space_is_illuminated
+Tests if can check if a space is illuminated or not
+
+@param
+@return
+*/
+void test2_space_is_illuminated(){
+	Space *space = NULL;
+  Id id = 1;
+  space = space_create(id);
+  space_set_illumination(space,TRUE);
+  FUNCTION_IS_CORRECT(space_is_illuminated(space) == TRUE);
+	return;
+}
+
+
+/**
 @author Adrián Fernández
 @date Nov. 23, 2016
 
@@ -1134,3 +1381,6 @@ void test2_space_print(){
 	FUNCTION_IS_CORRECT(space_print(space) == ERROR);
 	return;
 }
+
+
+
